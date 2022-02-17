3f824095c6c0fee4248568f7eb45f24f5e04d456
lider <vadim4ik9@gmail.com>
2022-02-17 10:44:07 +0300

    Initial Commit

---
+++ coinWellHomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coinWellHomeWork
+//
+//  Created by Vadim on 17.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
